897863c526d9fb533dce6de9655ca2ecdb980466
ckolhen <nilesh.kolhe-ext@jci.com>
2017-01-02 14:07:25 +0530

    firstDemoCommit

---
+++ src/DemoJenkins/DemoJenkinJobs.java
@@ -0,0 +1,14 @@
+package DemoJenkins;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkinJobs {
+	
+	@Test
+	public void testJenkins(){
+		
+		System.out.println("Welcome to Jenkin world");
+		
+	}
+
+}
